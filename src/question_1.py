1e0d8241c5384ebd8514c61b749d090c48d00f85
danielbauwens <69080058+danielbauwens@users.noreply.github.com>
2023-08-31 15:38:15 +0200

    Move py files to src folder + create question_1.py

---
+++ src/question_1.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+import plotly.express as px
+
+query = """
+    SELECT wines.id AS wine_id, 
+           wines.name AS wine_name, 
+           wines.ratings_average AS wine_avg_rating, 
+           wines.ratings_count AS wine_ratings_count, 
+           COUNT(vintages.year) AS vintages_number, 
+           ROUND(AVG(vintages.price_euros), 2) AS average_price,
+           countries.name AS country_name
+    FROM wines
+        JOIN vintages ON wines.id = vintages.wine_id
+        JOIN regions ON wines.region_id = regions.id
+        JOIN countries ON regions.country_code = countries.code
+    WHERE wines.ratings_count > 10000
+    GROUP BY wines.id
+    ORDER BY wine_avg_rating DESC, wine_ratings_count DESC;
+"""
+query = pd.read_sql_query(query, connection)
+
+
+
+query2 = """
+    SELECT wines.id AS wine_id, 
+           wines.name AS wine_name, 
+           wines.ratings_average wine_avg_rating, 
+           wines.ratings_count wine_ratings_count, 
+           COUNT(vintages.year) AS vintages_number, 
+           ROUND(AVG(vintages.price_euros), 2) AS average_price,
+           countries.name AS country_name
+    FROM wines
+        JOIN vintages ON wines.id = vintages.wine_id
+        JOIN regions ON wines.region_id = regions.id
+        JOIN countries ON regions.country_code = countries.code
+    WHERE vintages.price_euros <= 50 AND
+          wines.ratings_count > 10000 
+    GROUP BY wines.id
+    ORDER BY wine_avg_rating DESC, 
+             wine_ratings_count DESC,
+             vintages.price_euros ASC
+    -- LIMIT 10
+    ;
+"""
+
+query2 = pd.read_sql_query(query2, connection)
+
+
+
+col1, col2 = st.columns(2)
+with col1:
+    st.dataframe(query)
+with col2:
+    st.dataframe(query2)
